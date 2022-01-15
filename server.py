04b3d01f9bd734fa544d856fa4a6446fa038ddf4
Karol <sore.akarin@gmail.com>
2022-01-15 19:06:01 +0100

    Back-end mod

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ import time
 import math
 from threading import *
 
+# Info: the server must be ran as root.
 IN = 0
 OUT = 1
 LO = 0
 HI = 1
+
 
 # CONFIG
 # 	GPIO Access Control List
@@ -16,14 +18,13 @@ gpioacl = {23: (OUT, LO), 22: (OUT, LO)}
 
 # IP Address for the server to listen on
 iface = '0.0.0.0'
-# Note: for ports < 1024 the server script must be ran as root.
 port = 8080
 # END of CONFIG
 
 
+
 chip = dio.gpiochip_open(0)
 for key in gpioacl:
-    #dio.setup(key, gpioacl[key][0], initial=gpioacl[key][1])
     if gpioacl[key][0] == 1:
         dio.gpio_claim_output(chip, key, level=gpioacl[key][1])
     else:
@@ -111,32 +112,57 @@ class Client(Thread):
         try:
             par = req[req.find("?")+1:]
             spl = par.split("&")
+
+            params = ""
+            split = []
+
             for param in spl:
+                if not param.startswith("gpio"):
+                    self.res(
+                        f"<li class='error'><p>Invalid param \"{param}\" - skip</p></li>")
+                    continue
+                if "=" in param:
+                    a, b = param.split("=", 1)
+                    try:
+                        a = int(a[4:])
+                        if not a in gpioacl:
+                            self.res(
+                                f"<li class='error'><p>Forbidden GPIO {a} - skip</p></li>")
+                            continue
+                        b = int(b)
+                    except Exception as e:
+                        self.res(
+                            f"<li class='error'><p>Number format error: \"{e}\" - skip</p></li>")
+                else:
+                    a = param
+                    try:
+                        a = int(a[4:])
+                        if not a in gpioacl:
+                            self.res(
+                                f"<li class='error'><p>Forbidden GPIO {a} - skip</p></li>")
+                            continue
+                        b = dio.gpio_read(chip, a)
+                        param = f"{param}={b}"
+                    except Exception as e:
+                        self.res(
+                            f"<li class='error'><p>Error while figuring out param: \"{e}\" - skip</p></li>")
+                        continue
+                split.append(param)
+                params += param + "&"
+            params = params[:-1]
+            for param in split:
                 a, b = param.split("=", 1)
-                if not a.startswith("gpio"):
-                    self.res(
-                        f"<li class='error'><p>Invalid param \"{a}\" - skip</p></li>")
-                    continue
-                try:
-                    a = int(a[4:])
-                    b = int(b)
-                except Exception as e:
-                    self.res(
-                        f"<li class='error'><p>Number format error: \"{e}\" - skip</p></li>")
-                    continue
-                if not a in gpioacl:
-                    self.res(
-                        f"<li class='error'><p>Forbidden GPIO {a} - skip</p></li>")
-                    continue
+                a = int(a[4:])
+                b = int(b)
                 if gpioacl[a][0] == OUT:
                     if b <= 0:
                         dio.gpio_write(chip, a, LO)
                         self.res(
-                            f'<li><a href="?{par.replace(param, param[:-1] + "1")}"><div class="control-on">GPIO {a}: <span class="green">ON</span></div></a></li>')
+                            f'<li><a href="?{params.replace(param, params[:-1] + "1")}"><div class="control-on">GPIO {a}: <span class="green">ON</span></div></a></li>')
                     else:
                         dio.gpio_write(chip, a, HI)
                         self.res(
-                            f'<li><a href="?{par.replace(param, param[:-1] + "0")}"><div class="control-off">GPIO {a}: <span class="red">OFF</span></div></a></li>')
+                            f'<li><a href="?{params.replace(param, params[:-1] + "0")}"><div class="control-off">GPIO {a}: <span class="red">OFF</span></div></a></li>')
                     continue
                 self.res(
                     f"<li class='error'><p>Bad GPIO mode for {a} - skip</p></li>")
@@ -154,7 +180,6 @@ srv.setsockopt(sck.SOL_SOCKET, sck.SO_REUSEADDR, 1)
 srv.bind((iface, port))
 srv.listen()
 print(f"Started listening on /{iface}:{port}")
-
 
 while True:
     try:
